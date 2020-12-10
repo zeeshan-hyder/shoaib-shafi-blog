e6aa1f8935cde5244753f3022da09fdbba458820
sha337 <shabbaralee@gmail.com>
2020-12-10 16:17:33 +0530

    added different pages

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,31 @@ const express = require("express");
 const router  = express.Router();
 
 router.get('/', (req,res) => {
-    res.render("home");
+    let title = {
+        text: "Shoaib Shafi - Freelance Writer"
+    }
+    res.render("home", {title});
+});
+
+router.get('/about', (req,res) => {
+    let title = {
+        text: "ABOUT - Shoaib Shafi"
+    }
+    res.render("about", {title});
+});
+
+router.get('/articles', (req, res) => {
+    let title = {
+        text: "ARTICLES & ESSAYS - Shoaib Shafi"
+    }
+    res.render("articles", {title});
+});
+
+router.get('/contact', (req,res) => {
+    let title = {
+        text: "CONTACT - Shoaib Shafi"
+    }
+    res.render("contact",{title});
 });
 
 
